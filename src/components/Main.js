010d46807d95b3ecf4648e89bec01c81f843ddac
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-04-08 14:13:51 -0300

    Projeto Lista de Tarefas,utilizando React

---
+++ src/components/Main.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './Main.css';
+// form
+import { FaPlus } from 'react-icons/fa';
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+    tarefas: ['Fazer Café', 'Beber água', 'Estudar'],
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  }
+
+  render() {
+    const { novaTarefa, tarefas } = this.state;
+    return (
+      <div className="main">
+        <h1>Lista de Tarefas</h1>
+
+        <form action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
+          <button type="submit">
+            <FaPlus />
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
